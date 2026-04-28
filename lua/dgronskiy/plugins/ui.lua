95dad959313eebda35b6a5b1be10f2c919411731
dgronskij <gronsky.dm@gmail.com>
2026-04-28 18:36:28 +0300

    tmux integration

--- lua/dgronskiy/plugins/ui.lua
+++ lua/dgronskiy/plugins/ui.lua
@@ -25,6 +25,25 @@ return {
         "norcalli/nvim-colorizer.lua",
         event = "VeryLazy",
     },
+    {
+        "mrjones2014/smart-splits.nvim",
+        lazy = false,
+        config = function()
+            require("smart-splits").setup()
+        end,
+        keys = {
+            -- Move between splits
+            { "<C-h>", function() require("smart-splits").move_cursor_left() end,  desc = "Move to left split" },
+            { "<C-j>", function() require("smart-splits").move_cursor_down() end,  desc = "Move to below split" },
+            { "<C-k>", function() require("smart-splits").move_cursor_up() end,    desc = "Move to above split" },
+            { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
+            -- Resize splits
+            { "<C-Left>",  function() require("smart-splits").resize_left() end,  desc = "Resize split left" },
+            { "<C-Down>",  function() require("smart-splits").resize_down() end,  desc = "Resize split down" },
+            { "<C-Up>",    function() require("smart-splits").resize_up() end,    desc = "Resize split up" },
+            { "<C-Right>", function() require("smart-splits").resize_right() end, desc = "Resize split right" },
+        },
+    },
     -- {
     --     "hat0uma/csvview.nvim",
     --     ---@module "csvview"
