c022a8394bcb559060119ddb62336824e2feb333
dgronskij <gronsky.dm@gmail.com>
2026-05-07 19:58:02 +0300

    .

--- lua/dgronskiy/setup_autocmds.lua
+++ lua/dgronskiy/setup_autocmds.lua
@@ -9,3 +9,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+-- Restore last cursor position when reopening a known file
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Restore last cursor position when opening a file',
+  group = vim.api.nvim_create_augroup('restore-cursor', { clear = true }),
+  callback = function(args)
+    local buf = args.buf
+    -- guard against double-firing and skip filetypes where restoring makes no sense
+    if vim.b[buf].last_loc_restored or vim.tbl_contains({ 'gitcommit' }, vim.bo[buf].filetype) then return end
+    vim.b[buf].last_loc_restored = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(buf) then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
